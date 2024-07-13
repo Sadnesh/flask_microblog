b93817271fc14abba67e33af014e6073a513cc4d
sandesh bhantana <sandeshbhantana@gmail.com>
2024-07-13 16:19:09 +0545

    [add templates, form and config(example)]

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
+from .forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "Sadnesh"}
+    posts = [
+        {"author": {"username": "someone"}, "body": "It's a wonderful day"},
+        {"author": {"username": "noone"}, "body": "It's a shitty day"},
+        {"author": {"username": "somebody"}, "body": "It's a joyful today"},
+    ]
+    return render_template("index.html", title="Blog Testing", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Log In", form=form)
