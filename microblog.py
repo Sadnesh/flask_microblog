3f392aeb9698acbb9a8d40be8d94e6b10137a3fc
sandesh bhantana <sandeshbhantana@gmail.com>
2024-07-14 21:49:23 +0545

    [feat: add shell command for easy db access]

--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
-from app import app
+import sqlalchemy as sal
+import sqlalchemy.orm as sorm
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sal": sal, "sorm": sorm, "db": db, "User": User, "Post": Post}
