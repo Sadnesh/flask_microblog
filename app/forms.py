04ddb6b5d1cd91d11f680ffbb12bac4737bd0075
sandesh bhantana <sandeshbhantana@gmail.com>
2024-07-18 22:35:38 +0545

    [add: feature for profile editing]

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from app import db
 import sqlalchemy as sal
 from app.models import User
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 
 
 class LoginForm(FlaskForm):
@@ -22,13 +22,18 @@ class RegistrationForm(FlaskForm):
     )
     submit = SubmitField("Register")
 
-
-    def validate_username(self,username):
-        user=db.session.scalar(sal.select(User).where(User.username==username.data))
+    def validate_username(self, username):
+        user = db.session.scalar(sal.select(User).where(User.username == username.data))
         if user is not None:
             raise ValidationError("Please use a different username.")
 
-    def validate_email(self,email):
-        user=db.session.scalar(sal.select(User).where(User.email==email.data))
+    def validate_email(self, email):
+        user = db.session.scalar(sal.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError("Please use a different email address.")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
