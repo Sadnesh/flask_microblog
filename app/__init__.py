4852d614dbc5323bc0f3fd1e5ae771315f516e69
sandesh bhantana <sandeshbhantana@gmail.com>
2024-07-24 22:26:49 +0545

    [update: better time in profile] [add: time to posts]

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import logging
 from flask import Flask
 from config import Config
 from flask_mail import Mail
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -13,6 +14,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 mail = Mail(app)
 db = SQLAlchemy(app)
+moment = Moment(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"  # type:ignore
