c77931055113bba68f8271cacbae648d92673204
sandesh bhantana <sandeshbhantana@gmail.com>
2024-07-30 12:22:02 +0545

    [update: missed refactor in top level script]

--- microblog.py
+++ microblog.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sal
 import sqlalchemy.orm as sorm
-from app import app, db, cli
+from app import current_app, db, cli
 from app.models import User, Post
 
 
-@app.shell_context_processor
+@current_app.shell_context_processor
 def make_shell_context():
     return {"sal": sal, "sorm": sorm, "db": db, "User": User, "Post": Post}
